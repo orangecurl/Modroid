04fc422da6313e86045647bd28ff295a4af82306
Arifin Paliwang <paliwangzainal05@gmail.com>
2024-12-17 07:52:39 +0700

    Create api_service.dart

---
+++ lib/services/api_service.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiService {
+  final String baseUrl;
+
+  ApiService({required this.baseUrl});
+
+  Future<List<User>> fetchUsers() async {
+    final response = await http.get(Uri.parse('$baseUrl/users'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((user) => User(name: user['name'], email: user['email'])).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+}
